42859adfe6b96dee788fe887fe9417ff8cc19a50
TetyanaGamma <tetyanagamma@gmail.com>
2025-11-22 20:07:53 +0400

    Навигация: созданы фрагменты для основных экранов

---
+++ app/src/main/java/ru/practicum/android/diploma/presentation/filters/FiltersIndustriesFragment.kt
@@ -0,0 +1,21 @@
+package ru.practicum.android.diploma.presentation.filters
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ru.practicum.android.diploma.databinding.FragmentFiltersIndustriesBinding
+
+class FiltersIndustriesFragment:Fragment() {
+
+    private var _binding: FragmentFiltersIndustriesBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = FragmentFiltersIndustriesBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+}
---
+++ app/src/main/java/ru/practicum/android/diploma/presentation/search/SearchFragment.kt
@@ -0,0 +1,28 @@
+package ru.practicum.android.diploma.presentation.search
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ru.practicum.android.diploma.databinding.FragmentSerchBinding
+
+class SearchFragment: Fragment() {
+
+    private var _binding: FragmentSerchBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSerchBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnVacancies.setOnClickListener {  }
+        binding.btnFilters.setOnClickListener {  }
+    }
+}
