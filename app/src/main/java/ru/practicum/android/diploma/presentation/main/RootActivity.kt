f0bb73cba8efc6bd47f4c26208a4bcac5d1fe628
TetyanaGamma <tetyanagamma@gmail.com>
2025-11-23 16:23:22 +0400

    BottomNavigationView сделан работает

--- app/src/main/java/ru/practicum/android/diploma/presentation/main/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/main/RootActivity.kt
@@ -3,6 +3,7 @@ package ru.practicum.android.diploma.presentation.main
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import ru.practicum.android.diploma.BuildConfig
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.ActivityRootBinding
@@ -13,6 +14,13 @@ class RootActivity : AppCompatActivity() {
         ActivityRootBinding.inflate(layoutInflater)
     }
 
+    // Список id фрагментов, где BottomNavigationView должен быть виден
+    private val bottomNavVisibleDestinations = setOf(
+        R.id.searchFragment,
+        R.id.favoriteVacancyFragment,
+        R.id.teamFragment
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -22,11 +30,32 @@ class RootActivity : AppCompatActivity() {
         ) as NavHostFragment
         val navController = navHostFragment.navController
 
+        // Подключаем BottomNavigationView к NavController
+        binding.bottomNavigationView.setupWithNavController(navController)
+
+        // Слушаем изменения фрагмента и скрываем/показываем меню
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id in bottomNavVisibleDestinations) {
+                binding.bottomNavigationView.showNav()
+            } else {
+                binding.bottomNavigationView.hideNav()
+            }
+        }
+
         // Пример использования access token для HeadHunter API
         networkRequestExample(accessToken = BuildConfig.API_ACCESS_TOKEN)
     }
 
     private fun networkRequestExample(accessToken: String) {
         // ...
+    }
+
+    // Функции расширения для показа/скрытия BottomNavigationView
+    private fun com.google.android.material.bottomnavigation.BottomNavigationView.showNav() {
+        this.visibility = android.view.View.VISIBLE
+    }
+
+    private fun com.google.android.material.bottomnavigation.BottomNavigationView.hideNav() {
+        this.visibility = android.view.View.GONE
     }
 }
