fc73dc304973c46325df7dbbe2c4e538a586bdfa
Daniel Rotman <danilarothmann@gmail.com>
2025-11-26 21:53:18 +0700

    Epic1 Res rename res remote log

--- app/src/main/java/ru/practicum/android/diploma/App.kt
+++ app/src/main/java/ru/practicum/android/diploma/App.kt
@@ -9,14 +9,6 @@ import ru.practicum.android.diploma.di.appModule
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
-
-        // ПРОВЕРКА ТОКЕНА
-        Log.d("TOKEN_DEBUG", "=== TOKEN CHECK ===")
-        Log.d("TOKEN_DEBUG", "Token: ${BuildConfig.API_ACCESS_TOKEN}")
-        Log.d("TOKEN_DEBUG", "Token length: ${BuildConfig.API_ACCESS_TOKEN.length}")
-        Log.d("TOKEN_DEBUG", "First 10 chars: ${BuildConfig.API_ACCESS_TOKEN.take(10)}...")
-        Log.d("TOKEN_DEBUG", "===================")
-
         startKoin {
             androidContext(this@App)
             modules(appModule)
