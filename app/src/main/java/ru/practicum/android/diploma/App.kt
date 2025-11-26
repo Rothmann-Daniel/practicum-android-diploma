4bb3ed277bf379d0df3ec88a85d8e2cd485b4516
Daniel Rotman <danilarothmann@gmail.com>
2025-11-26 21:51:03 +0700

    Epic1 Res rename res v2

--- app/src/main/java/ru/practicum/android/diploma/App.kt
+++ app/src/main/java/ru/practicum/android/diploma/App.kt
@@ -1,6 +1,7 @@
 package ru.practicum.android.diploma
 
 import android.app.Application
+import android.util.Log
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.GlobalContext.startKoin
 import ru.practicum.android.diploma.di.appModule
@@ -8,6 +9,14 @@ import ru.practicum.android.diploma.di.appModule
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        // ПРОВЕРКА ТОКЕНА
+        Log.d("TOKEN_DEBUG", "=== TOKEN CHECK ===")
+        Log.d("TOKEN_DEBUG", "Token: ${BuildConfig.API_ACCESS_TOKEN}")
+        Log.d("TOKEN_DEBUG", "Token length: ${BuildConfig.API_ACCESS_TOKEN.length}")
+        Log.d("TOKEN_DEBUG", "First 10 chars: ${BuildConfig.API_ACCESS_TOKEN.take(10)}...")
+        Log.d("TOKEN_DEBUG", "===================")
+
         startKoin {
             androidContext(this@App)
             modules(appModule)
