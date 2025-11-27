ed5573e809b1c3d8d04f6179d499c91d0cb82951
TetyanaGamma <tetyanagamma@gmail.com>
2025-11-27 22:58:17 +0400

    Поиск с исправлениями detek 2

--- app/src/main/java/ru/practicum/android/diploma/App.kt
+++ app/src/main/java/ru/practicum/android/diploma/App.kt
@@ -5,6 +5,7 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.GlobalContext.startKoin
 import ru.practicum.android.diploma.di.appModule
 
+
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
