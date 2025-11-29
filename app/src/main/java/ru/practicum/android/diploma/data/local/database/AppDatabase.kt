2605271779cea82f39fd31e6ad43cc81f3f1a743
AlexeyNenashev <en169@yandex.ru>
2025-11-29 13:52:19 +0700

    добавил entity и dao для избранных вакансий (favorites), поднял версию БД до 3

--- app/src/main/java/ru/practicum/android/diploma/data/local/database/AppDatabase.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/local/database/AppDatabase.kt
@@ -5,19 +5,22 @@ import androidx.room.RoomDatabase
 import ru.practicum.android.diploma.data.local.dao.AreaDao
 import ru.practicum.android.diploma.data.local.dao.IndustryDao
 import ru.practicum.android.diploma.data.local.dao.VacancyDao
+import ru.practicum.android.diploma.data.local.dao.VacancyInFavoritesDao
 import ru.practicum.android.diploma.data.local.entities.AreaEntity
 import ru.practicum.android.diploma.data.local.entities.IndustryEntity
 import ru.practicum.android.diploma.data.local.entities.VacancyEntity
+import ru.practicum.android.diploma.data.local.entities.VacancyInFavoritesEntity
 
 @Database(
-    entities = [AreaEntity::class, IndustryEntity::class, VacancyEntity::class],
-    version = 2,
+    entities = [AreaEntity::class, IndustryEntity::class, VacancyEntity::class, VacancyInFavoritesEntity::class],
+    version = 3,
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
     abstract fun areaDao(): AreaDao
     abstract fun industryDao(): IndustryDao
     abstract fun vacancyDao(): VacancyDao
+    abstract fun vacancyInFavoritesDao(): VacancyInFavoritesDao
 
     companion object {
         const val DATABASE_NAME = "practicum_db"
